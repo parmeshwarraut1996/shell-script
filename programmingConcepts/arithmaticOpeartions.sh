15b3666e4d627f5f57b3778f425d2df90c68f017
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2020-02-11 13:13:25 +0530

    [Parmeshwar]Added. sequenceNumber.sh in code base

---
+++ programmingConcepts/arithmaticOpeartions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+read -p "Enter value of a , b & c = " a b c
+
+echo "1)a + b * c =$(( a + b * c )) "
+echo "2)c + a / b =$(( c + a / b )) "
+echo "3)a % b + c =$(( a % b + c )) "
+echo "4)a * b + c =$(( a * b + c )) " 
---
+++ programmingConcepts/sequenceNumber.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+random1=` expr 1 + $RANDOM % 60 `
+
